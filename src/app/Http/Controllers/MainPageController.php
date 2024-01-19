e27f6ccb225a3c7ff005353e98093c21a334c0cd
KhirovSasha <sasha.khiro@gmail.com>
2024-01-19 22:42:17 +0200

    created service for work with comments and implement in MainPageController

--- src/app/Http/Controllers/MainPageController.php
+++ src/app/Http/Controllers/MainPageController.php
@@ -2,13 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CommentResource;
+use App\Http\Resources\UserResource;
+use App\Http\Services\CommentServices;
+use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class MainPageController extends Controller
 {
-    function index()
+    protected $commentServices;
+
+    public function __construct(CommentServices $commentServices)
     {
-        return Inertia::render('create');
+        $this->commentServices = $commentServices;
     }
+
+    public function index()
+    {
+        $sortQuery = request()->query('sort');
+        $urlWithParameters = request()->getRequestUri();
+
+        $commentsResult = $this->commentServices->prepareCommentQueryWithSort($sortQuery, $urlWithParameters);
+
+        $comments = $commentsResult['comments']->paginate(25);
+
+        return Inertia::render('MainePage', [
+            'comments' => CommentResource::collection($comments),
+            'sort' => $commentsResult['sort'],
+            'urlWithParameters' => $commentsResult['urlWithParameters'],
+            'sortAttribute' => $commentsResult['sortAttribute']
+        ]);
+    }
+
+
+
 }
