a36c8600caa6fbe6164d085b5dcfbd2ef542b5e7
KhirovSasha <sasha.khiro@gmail.com>
2024-01-19 17:55:12 +0200

    clearing project test folder

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -12,4 +12,9 @@ class Comment extends Model
     protected $fillable = [
         'text',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userID');
+    }
 }
