1b035adbee9fc9c5132b5f5f91e01f87e986265d
KhirovSasha <sasha.khiro@gmail.com>
2024-01-20 03:00:06 +0200

    changing showed time, added saving url when user click on last or fist navigation button and changed creating url for sorting

--- src/resources/js/Pages/MainPage.vue
+++ src/resources/js/Pages/MainPage.vue
@@ -1,18 +1,16 @@
 <script setup>
 import Pagination from "@/Components/Pagination.vue";
 import { defineProps, ref, onMounted } from 'vue';
-import {Link} from '@inertiajs/vue3'
+import {Link} from '@inertiajs/vue3';
+import { format, differenceInHours, parseISO } from 'date-fns';
 
 const { comments, urlWithParameters, sort, sortAttribute } = defineProps(['comments', 'urlWithParameters', 'sort', 'sortAttribute']);
 
-//$sortUrl = ref("{$comments.meta.path}/?page={$comments.meta.current_page}&sort=" . ($sort === 'ASC' && $sortAttribute === 'userName' ? '-userName' : 'userName'));
-
-const sortUrl = ref((sort === 'ASC' && sortAttribute === 'userName') ? `${comments.meta.path}/?page=${comments.meta.current_page}&sort=-userName` : `${comments.meta.path}/?page=${comments.meta.current_page}&sort=userName`);
-const sortEmail = ref((sort === 'ASC' && sortAttribute === 'email') ? `${comments.meta.path}/?page=${comments.meta.current_page}&sort=-email` : `${comments.meta.path}/?page=${comments.meta.current_page}&sort=email`);
-const sortCrateAt = ref((sort === 'ASC' && sortAttribute === 'created_at') ? `${comments.meta.path}/?page=${comments.meta.current_page}&sort=-created_at` : `${comments.meta.path}/?page=${comments.meta.current_page}&sort=created_at`);
+const sortUrl = ref(`${comments.meta.path}/?page=${comments.meta.current_page}&sort=${sort === 'ASC' && sortAttribute === 'userName' ? '-userName' : 'userName'}`);
+const sortEmail = ref(`${comments.meta.path}/?page=${comments.meta.current_page}&sort=${sort === 'ASC' && sortAttribute === 'email' ? '-email' : 'email'}`);
+const sortCrateAt = ref(`${comments.meta.path}/?page=${comments.meta.current_page}&sort=${sort === 'ASC' && sortAttribute === 'created_at' ? '-created_at' : 'created_at'}`);
 
 onMounted(() =>{
-    console.log(urlWithParameters);
     if(urlWithParameters != null) {
         comments.meta.links.forEach((e, index) => {
 
@@ -20,9 +18,39 @@ onMounted(() =>{
                 const parts = urlWithParameters.split('&');
                 e.url = comments.meta.path +  parts[0].slice(0, -1)+e.label + '&' + parts[1];
             }
+
         })
+
+        if(comments.meta.links[0].url == null){
+            comments.meta.links[comments.meta.links.length - 1].url = modifyUrlLastChar('increment');
+        } else {
+            comments.meta.links[0].url = modifyUrlLastChar('decrement');
+        }
     }
 })
+
+const formatDateOrTime = (createdAt) => {
+    const parsedDate = parseISO(createdAt);
+    const currentTime = new Date();
+    const hoursDifference = differenceInHours(currentTime, parsedDate);
+
+    if (hoursDifference < 24) {
+        return format(parsedDate, 'HH:mm:ss');
+    } else {
+        return format(parsedDate, 'dd/MM/yyyy');
+    }
+};
+
+const modifyUrlLastChar = (operation) => {
+    const parts = urlWithParameters.split('&');
+    const lastChar = parts[0].charAt(parts[0].length - 1);
+
+    const modifiedLastChar = operation === 'increment' ? parseInt(lastChar, 10) + 1 : parseInt(lastChar, 10) - 1;
+    const modifiedFirstPart = parts[0].slice(0, -1) + modifiedLastChar;
+
+    return comments.meta.path + modifiedFirstPart + '&' + parts[1];
+};
+
 </script>
 
 <template>
@@ -47,7 +75,7 @@ onMounted(() =>{
                             <td>{{ comment.userName }}</td>
                             <td>{{ comment.userEmail }}</td>
                             <td>{{ comment.text }}</td>
-                            <td>{{ comment.createdAt }}</td>
+                            <td>{{ formatDateOrTime(comment.createdAt) }}</td>
                         </tr>
                     </table>
                 </div>
