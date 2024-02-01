d2559654ed0c3e520a146c3f7cd2c9d68cb5e53d
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-01 14:40:09 +0200

    added routes for reload and validate captcha

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CaptchaController;
 use App\Http\Controllers\MainPageController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [MainPageController::class, 'index']);
+
+Route::get('/reload-captcha', [CaptchaController::class, 'reloadCaptcha']);
+Route::post('/captcha-validation', [CaptchaController::class, 'capthcaFormValidate']);
