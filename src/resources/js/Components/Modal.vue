6d8a70d91a2162b80dc67d192a074d7859481260
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-01 14:41:04 +0200

    added csrf token and UI functionals for validate captcha

--- src/resources/js/Components/Modal.vue
+++ src/resources/js/Components/Modal.vue
@@ -27,10 +27,10 @@
                         <span class="sr-only">Close modal</span>
                     </button>
                 </div>
-                <form>
+                <form @submit.prevent="submitForm">
                     <!-- Modal body -->
                     <div class="p-4 md:p-5 space-y-4">
-
+                        <div v-html="errorMessages" class="text-red-500"></div>
                         <div class="mb-4">
                             <label for="username" class="block text-sm font-medium text-gray-700 dark:text-white">
                                 User Name
@@ -53,12 +53,36 @@
                             <label for="homepage" class="block text-sm font-medium text-gray-700 dark:text-white">
                                 Home page
                             </label>
-                            <input type="url" id="homepage" name="homepage"
+                            <input type="text" id="homepage" name="homepage"
                                 class="mt-1 p-2 w-full border border-gray-300 rounded-md dark:bg-gray-800 dark:border-gray-700">
                         </div>
 
                         <!-- img -->
-                        <img :src="captcha" v-if="captcha" />
+                        <div class="mb-4">
+                            <span><img :src="reactiveCaptcha" v-if="reactiveCaptcha" /></span>
+                            <button type="button" @click="reloadCaptcha" class="btn btn-danger" id="reload">
+                                &#x21bb;
+                            </button>
+
+                            <input type="text" id="captcha" placeholder="Enter Captcha" name="captcha"
+                                class="mt-1 p-2 w-full border border-gray-300 rounded-md dark:bg-gray-800 dark:border-gray-700">
+                        </div>
+
+                        <div class="mb-4">
+
+                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                message</label>
+                            <div class="flex space-x-4 mt-4">
+                                <button @click.prevent="insertTag('i')" class="btn btn-primary">Italic</button>
+                                <button @click.prevent="insertTag('strong')" class="btn btn-primary">Bold</button>
+                                <button @click.prevent="insertTag('code')" class="btn btn-primary">Code</button>
+                                <button @click.prevent="insertTag('a')" class="btn btn-primary">Add Link</button>
+                            </div>
+
+                            <textarea id="message" rows="4" name="message"
+                                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Write your thoughts here..."></textarea>
+                        </div>
 
                     </div>
                     <!-- Modal footer -->
@@ -77,10 +101,60 @@
 
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 
-defineProps({
-    captcha: String
-})
+let { captcha } = defineProps(['captcha']);
+const reactiveCaptcha = ref(captcha);
+const errorMessages = ref('');
 
+const reloadCaptcha = () => {
+    axios.get('/reload-captcha')
+        .then(response => {
+            reactiveCaptcha.value = response.data.captcha;
+        })
+        .catch(error => {
+            console.error('Помилка при виконанні AJAX-запиту:', error);
+        });
+};
+
+const submitForm = () => {
+    const formData = new FormData(document.querySelector('form'));
+
+    formData.append('_token', csrf);
+
+    axios.post('/captcha-validation', formData)
+        .then(response => {
+            console.log('Form submitted successfully!', response);
+            errorMessages.value = '';
+        })
+        .catch(error => {
+            if (error.response && error.response.data && error.response.data.errors) {
+                const errors = error.response.data.errors;
+                const errorMessageArray = Object.values(errors).flat();
+                errorMessages.value = errorMessageArray.join('<br>');
+            } else {
+                errorMessages.value = 'An unexpected error occurred.';
+            }
+            console.error('Error submitting form:', error);
+
+            reloadCaptcha();
+        });
+};
+
+const csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 const showModal = ref(false);
+
+const insertTag = (tag) => {
+    const messageInput = document.getElementById('message');
+    const cursorPos = messageInput.selectionStart;
+    const textBeforeCursor = messageInput.value.substring(0, cursorPos);
+    const textAfterCursor = messageInput.value.substring(cursorPos);
+    if (tag == 'a') {
+        messageInput.value = textBeforeCursor + '<' + tag + ' href="" title="">' + '</' + tag + '>' + textAfterCursor;
+    } else {
+        messageInput.value = textBeforeCursor + '<' + tag + '>' + '</' + tag + '>' + textAfterCursor;
+    }
+    messageInput.setSelectionRange(cursorPos + tag.length, cursorPos + tag.length);
+    messageInput.focus();
+};
 </script>
