2a465a90a4e8ea8483171150402ef6bf95733fa2
Khirov Sasha <75026042+KhirovSasha@users.noreply.github.com>
2024-02-15 02:41:53 +0200

    Created simple MainePage

---
+++ src/resources/js/pages/MainePage.vue
@@ -0,0 +1,41 @@
+<script>
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+import {setupAxiosInterceptors} from '../helpers/axiosInterceptors.js'
+
+export default {
+    setup() {
+        setupAxiosInterceptors();
+
+        onMounted(() => {
+            axios.get('api/mainePage')
+                .then(res => {
+                    console.log(res);
+                }).catch(err => {
+                    console.log(err)
+                })
+        })
+
+        return {};
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
